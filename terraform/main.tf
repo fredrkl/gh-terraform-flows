03e770d480d253e9d093a525cd0e08ab1d539049
Fredrik Klingenberg <fredrkl@gmail.com>
2023-10-21 07:50:56 +0200

    feat: using precondition
    Signed-off-by: Fredrik Klingenberg <fredrkl@gmail.com>

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,16 +8,43 @@ provider "azurerm" {
 #}
 # Another comment to invalidate the cache
 
-resource "azurerm_resource_group" "example-adding" {
-  name     = "adding-resource-group"
-  location = "eastus"
 
+#resource "azurerm_container_registry" "balbla" {
+#  location = "eastus"
+#  resource_group_name = azurerm_resource_group.example-adding.name
+#  sku = "Basic"
+#  name = "example-container-registry"
+#  admin_enabled = true
+#  lifecycle {
+#    precondition {
+#      condition     = self.location == "eastus"
+#      error_message = "The resource group must be located in the eastus region."
+#    }
+#    postcondition {
+#      condition = admin_enabled == false
+#      error_message = "Admin must be enabled"
+#    }
+#  }
+#}
+
+resource "azurerm_storage_account" "example-adding-storage" {
+  location                 = "eastus"
+  name                     = "examplestorageaccount"
+  account_replication_type = "LRS"
+  resource_group_name      = azurerm_resource_group.example-adding.name
+  account_tier             = "Standard"
   lifecycle {
     precondition {
-      condition     = self.location == "centralus"
-      error_message = "The resource group must be located in the eastus region."
+      condition     = azurerm_resource_group.example-adding.location == "eastus"
+      error_message = "The selected resource group must be located in the eastus region."
     }
   }
+}
+
+resource "azurerm_resource_group" "example-adding" {
+  name     = "adding-resource-group"
+  location = "northeurope"
+
 }
 
 terraform {
