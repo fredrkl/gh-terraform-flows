88bad13a28d2c546edd97b05666890d3f456005d
Fredrik Klingenberg <fredrkl@gmail.com>
2023-10-22 15:13:38 +0200

    fix: missing version on required provider
    Signed-off-by: Fredrik Klingenberg <fredrkl@gmail.com>

--- terraform/main.tf
+++ terraform/main.tf
@@ -31,7 +31,8 @@ terraform {
   required_version = ">= 1.6"
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
+      version = "3.77.0"
     }
   }
   backend "azurerm" {
