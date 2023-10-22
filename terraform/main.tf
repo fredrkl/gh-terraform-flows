010dfb8a0222d9428153f641f0e1b7a7fca063aa
Fredrik Klingenberg <fredrkl@gmail.com>
2023-10-22 11:38:51 +0200

    feat: using latest azurerm provider
    Signed-off-by: Fredrik Klingenberg <fredrkl@gmail.com>

--- terraform/main.tf
+++ terraform/main.tf
@@ -31,8 +31,8 @@ terraform {
   required_version = ">= 1.6"
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "2.46.0"
+      source   = "hashicorp/azurerm"
+      features = {}
     }
   }
   backend "azurerm" {
