78cb0ebda63a74b89a7e96f31ab37cfc285cf89c
Fredrik Klingenberg <fredrkl@gmail.com>
2023-10-24 19:57:18 +0200

    feat: adding check example
    Signed-off-by: Fredrik Klingenberg <fredrkl@gmail.com>

--- terraform/main.tf
+++ terraform/main.tf
@@ -41,3 +41,14 @@ terraform {
     key                  = "terraform.tfstate"
   }
 }
+
+check "health_check" {
+  data "http" "terraform_io" {
+    url = "https://www.terraform.io"
+  }
+
+  assert {
+    condition     = data.http.terraform_io.status_code == 200
+    error_message = "${data.http.terraform_io.url} returned an unhealthy status code"
+  }
+}
