24fb3f87baf63436420c4364eaf560c2a5123730
Fredrik Klingenberg <fredrkl@gmail.com>
2023-10-24 20:02:47 +0200

    feat: disable status check
    Signed-off-by: Fredrik Klingenberg <fredrkl@gmail.com>

--- terraform/main.tf
+++ terraform/main.tf
@@ -42,13 +42,13 @@ terraform {
   }
 }
 
-check "health_check" {
-  data "http" "terraform_io" {
-    url = "https://www.terraform.io"
-  }
-
-  assert {
-    condition     = data.http.terraform_io.status_code == 200
-    error_message = "${data.http.terraform_io.url} returned an unhealthy status code"
-  }
-}
+#check "health_check" {
+#  data "http" "terraform_io" {
+#    url = "https://www.terraform.io"
+#  }
+#
+#  assert {
+#    condition     = data.http.terraform_io.status_code == 200
+#    error_message = "${data.http.terraform_io.url} returned an unhealthy status code"
+#  }
+#}
